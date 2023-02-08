00b7092c4c73734068698255592472fdcba5877c
Lon Beshiri <lonbeshiri@gmail.com>
2023-02-08 11:32:02 -0500

    fix: added height prop to page class

--- src/Registration.tsx
+++ src/Registration.tsx
@@ -1,18 +1,11 @@
-import { pub } from '@sone-dao/sone-react-utils'
 import { Page } from '@sone-dao/tone-react-containers'
-import React, { useEffect } from 'react'
+import React from 'react'
 import EmailPassphraseBox from './components/EmailPassphraseBox'
 import styles from './Registration.module.scss'
 
 export interface IRegistrationProps {}
 
 const Registration: React.FC<IRegistrationProps> = ({}) => {
-  useEffect(() => {
-    useEffect(() => {
-      pub('__TONE_EVENTS__', 'helmet.hide', true)
-      return () => pub('__TONE_EVENTS__', 'helmet.hide', false)
-    }, [])
-  }, [])
   return (
     <Page className={styles.page}>
       <EmailPassphraseBox />
